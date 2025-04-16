af3a793be8589c9bfc8dbbdf704ccba6228ca0fe
BeezMaster <gwjvanhattem@gmail.com>
2025-04-16 08:45:25 +0200

    use db:transaction to ensure data integrity

--- app/Actions/UpdateOrderAction.php
+++ app/Actions/UpdateOrderAction.php
@@ -9,34 +9,41 @@ use App\Models\Contact;
 use App\Models\Order;
 use BeezMaster\QLSClient\Requests\ValueObjects\ShipmentValueObject as ShipmentRequestValueObject;
 use BeezMaster\QLSClient\Responses\ValueObjects\ShipmentValueObject as ShipmentResponseValueObject;
+use Illuminate\Support\Facades\DB;
 
 class UpdateOrderAction extends AbstractOrderAction
 {
     public function execute(int $orderId, ShipmentRequestValueObject|ShipmentResponseValueObject $shipment): Order
     {
-        $orderLines = array_map(function ($item) {
-            return [
-                'name' => $item['name'],
-                'amount' => $item['amount'],
-                'price_per_unit' => $item['price_per_unit'],
-            ];
-        }, $this->getShipmentProducts($shipment));
+        return DB::transaction(function () use ($shipment, $orderId) {
+            $orderLines = array_map(function ($item) {
+                return [
+                    'name' => $item['name'],
+                    'amount' => $item['amount'],
+                    'price_per_unit' => $item['price_per_unit'],
+                ];
+            }, $this->getShipmentProducts($shipment));
 
-        $receiver = $this->getReceiverContact($shipment);
+            $receiver = $this->getReceiverContact($shipment);
 
-        $order = $this->getOrder($orderId);
-        $order->shipment_id = $this->getShipmentId($shipment);
-        $order->product_combination_id = $this->getProductCombinationId($shipment);
-        $order->update();
+            $order = $this->getOrder($orderId);
+            $order->shipment_id = $this->getShipmentId($shipment);
+            $order->product_combination_id = $this->getProductCombinationId($shipment);
+            $order->update();
 
-        /** @var Contact $contact */
-        $contact = $order->contacts?->where('type', '=', ContactTypeEnum::RECEIVER)->first();
-        $contact->update($receiver);
-        $order->orderLines()->delete();
-        $order->refresh();
+            /** @var Contact|null $contact */
+            $contact = $order->contacts()?->where('type', ContactTypeEnum::RECEIVER->value)->first();
 
-        $order->orderLines()->createMany($orderLines);
+            if ($contact) {
+                $contact->update($receiver);
+            }
 
-        return $order;
+            $order->orderLines()->delete();
+            $order->refresh();
+
+            $order->orderLines()->createMany($orderLines);
+
+            return $order;
+        });
     }
 }
